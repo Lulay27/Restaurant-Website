dd938b5a874e9493acb9d1e07567da20c1a76d15
Eric Le <ericle201727@gmail.com>
2022-05-28 02:06:17 -0700

    pushing from laptop b/c of weird error from query selecting .id instead of #id. This error caused the server to not update the browser and delete my bundle.js and create main.js and runtime.js. I resorted to just going on laptop that had an earlier version of the code and just make the changes (. replaced with #) withoutgetting an error

--- src/home.js
+++ src/home.js
@@ -3,12 +3,15 @@ const homePage = (() => {
     // creates element, class name, innerHTML, and appends
     const elementCreator = (name,type,text,parent) => {
         const temp = document.createElement(type);
-        temp.className = name;
+        temp.id = name;
         temp.innerHTML = text
         document.querySelector(parent).appendChild(temp);
     }
 
-    
+    // error from .id should be #id this caused the webpack dev server to delete bundle.js
+    // and create runtime.js and main.js not sure why something with chunks i think
+    // had to go on laptop with an earlier version of code and push cause im lazy lol
+
     elementCreator('','header','','#content');
     elementCreator('home','div','Home','header');
     elementCreator('menu','div','Menu','header');
@@ -16,21 +19,21 @@ const homePage = (() => {
     elementCreator('','main','','#content');
     elementCreator('main-img','img','','main');
     elementCreator('title-container','div','','main');
-    elementCreator('','h1','Mediocore Eats','.title-container');
+    elementCreator('','h1','Mediocore Eats','#title-container');
     elementCreator('text1-container','div','','main');
-    elementCreator('','p','Most Medicore food in town.','.text1-container');
-    elementCreator('','h3','-Eric','.text1-container');
+    elementCreator('','p','Most Medicore food in town.','#text1-container');
+    elementCreator('','h3','-Eric','#text1-container');
     elementCreator('text2-container','div','','main');
-    elementCreator('','h3','Sunday: 5pm - 9pm','.text2-container');
-    elementCreator('','ul','Monday: CLOSED','.text2-container');
-    elementCreator('','ul','Tuesday: CLOSED','.text2-container');
-    elementCreator('','ul','Wednesday: CLOSED','.text2-container');
-    elementCreator('','ul','Thursday: CLOSED','.text2-container');
-    elementCreator('','ul','Friday: 5pm - 9pm','.text2-container');
-    elementCreator('','ul','Saturday: 5pm - 9pm','.text2-container');
+    elementCreator('','h3','Sunday: 5pm - 9pm','#text2-container');
+    elementCreator('','ul','Monday: CLOSED','#text2-container');
+    elementCreator('','ul','Tuesday: CLOSED','#text2-container');
+    elementCreator('','ul','Wednesday: CLOSED','#text2-container');
+    elementCreator('','ul','Thursday: CLOSED','#text2-container');
+    elementCreator('','ul','Friday: 5pm - 9pm','#text2-container');
+    elementCreator('','ul','Saturday: 5pm - 9pm','#text2-container');
     elementCreator('text3-container','div','','main');
-    elementCreator('text3-container','h3','Location','.text3-container');
-    elementCreator('text3-container','div','308 Negra Arroyo Lane, Albuquerque, New Mexico','.text3-container');
+    elementCreator('text3-container','h3','Location','#text3-container');
+    elementCreator('text3-container','div','308 Negra Arroyo Lane, Albuquerque, New Mexico','#text3-container');
     elementCreator('','footer','','main');
     elementCreator('','div','Website created by Eric Le','footer');
 
