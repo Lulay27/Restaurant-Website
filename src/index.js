b71527458e24dd2340a47829ef7b892167207daa
SteezyLeezy <ericle201727@gmail.com>
2022-05-29 13:19:12 -0700

    removed msg.js and code from index.js associated with it do not need to test anymore and was just causing me confusion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import createDiv from "./msg";
 import "./styles/style.css";
 import Icon from './components/sunset-background.png';
 import homePage from './home.js'
 
-// createDiv('What a beautiful sunset - Lebron James');
 
-const testImg = document.createElement('img');
-testImg.className = 'testImg';
-testImg.src = Icon;
-// document.querySelector('.message').appendChild(testImg);
 
 
 
