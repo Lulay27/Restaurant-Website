ab12921c3be599c5fc54d0419fbb7bb2f34ac92c
Eric Le <ericle201727@gmail.com>
2022-06-01 17:56:38 -0700

    adding contact.js page

---
+++ src/contact.js
@@ -0,0 +1,10 @@
+import homePage from './home.js';
+
+const contactPage = (() => {
+
+    // homePage.elementCreator('contact-container','div','','#contact-header-page');
+
+
+})();
+
+export default contactPage;
